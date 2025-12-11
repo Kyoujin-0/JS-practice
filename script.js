e2ba4f31f27ed7cccbf43bd1505f2ad39d600903
Kyoujin <godyvirbinalay@gmail.com>
2025-12-12 06:52:01 +0800

    6:51

--- script.js
+++ script.js
@@ -5,10 +5,39 @@ const title = document.querySelector("#title")
 const subTitle = document.querySelectorAll("#subTitle")
 const sectionCard = document.querySelectorAll(".section-card")
 
+let darkMode = true
+
 toggle.addEventListener("click", ()=> {
-    body.style.background = "#f6f6f6"
+
+    if(darkMode) {
+        darkMode = false
+    body.style.background = "#ecececff"
     container.style.background = "white"
     title.style.color = "black"
+
+    subTitle.forEach(sub => {
+        sub.style.color = "black"
+    })
     
+    sectionCard.forEach(card => {
+        card.style.color = "black"
+        card.style.background = "#fbfafaff"
+    })
+    }
+    else {
+        darkMode = true
+    body.style.background = "#000000"
+    container.style.background = "rgb(54, 54, 54)"
+    title.style.color = "white"
+
+    subTitle.forEach(sub => {
+        sub.style.color = "white"
+    })
+    
+    sectionCard.forEach(card => {
+        card.style.color = "white"
+        card.style.background = "#797979"
+    })
+    }
 
 })
